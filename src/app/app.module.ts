af3c422b58679cfa1dea97fbc28102cb5835439a
oihoihoih <ochyuo@hotmail.com>
2024-07-17 12:05:26 +0200

    Project-item component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectItemComponent } from './project/project-item/project-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectItemComponent
   ],
   imports: [
     BrowserModule,
