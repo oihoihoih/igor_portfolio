6c106a8dad1e396a7ac2a7dc8a07f97006df2add
oihoihoih <ochyuo@hotmail.com>
2024-07-17 13:05:45 +0200

    Working in Project structure

--- src/app/model/project.ts
+++ src/app/model/project.ts
@@ -1,2 +1,12 @@
 export class Project {
+  constructor(
+    public name: string,
+    public year: number,
+    public director: string,
+    public DOP: string,
+    public imageUrl: string,
+    public genre: string,
+    public trailerUrl: string,
+    public imdbUrl: string
+  ) {}
 }
--- src/app/project/project-item/project-item.component.ts
+++ src/app/project/project-item/project-item.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../../model/project';
 
 @Component({
   selector: 'app-project-item',
   templateUrl: './project-item.component.html',
-  styleUrl: './project-item.component.css'
+  styleUrl: './project-item.component.css',
 })
-export class ProjectItemComponent {
+export class ProjectItemComponent implements OnInit {
+  // TODO: Crear una interfaz para el objeto proyecto. He creado un modelo, estudiar las diferencias
+  // TODO: Arreglar la propiedad opcional en las variables
 
+  public project?: Project;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.project = new Project(
+      'Proyecto 1',
+      2024,
+      'Director 1',
+      'DOP 1',
+      'https://via.placeholder.com/150',
+      'Género 1',
+      'https://www.youtube.com/watch?v=123456',
+      'https://www.imdb.com/title/tt123456'
+    );
+  }
+
+  closeButton(event: any) {
+    console.log('close', event);
+  }
 }
