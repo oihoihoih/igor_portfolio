4d5ac7e2c61a1a9892a6ea01449b3e5fcdf90927
oihoihoih <ochyuo@hotmail.com>
2024-08-10 11:44:14 +0200

    Dashboard routing defined

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: DashboardLayoutComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
