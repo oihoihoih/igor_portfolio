de92f9e809cb2f77e475334a8fc1b88fc40ac24d
oihoihoih <ochyuo@hotmail.com>
2024-09-06 13:26:09 +0200

    Reactive form in login component added

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Import Validators from '@angular/forms'
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+  public loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', { validators: [Validators.required, Validators.email] }],
+      password: [
+        '',
+        { validators: [Validators.required, Validators.minLength(6)] },
+      ],
+    });
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+  }
 }
