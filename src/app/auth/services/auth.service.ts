9884ecceb078c4a8e3dd72d5b6e96274f0af097c
oihoihoih <ochyuo@hotmail.com>
2024-09-20 17:22:23 +0200

    Logout implemented

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -51,7 +51,10 @@ export class AuthService {
     const url = `${this.baseUrl}/auth/check-token`;
     const token = localStorage.getItem('token');
 
-    if (!token) return of(false);
+    if (!token) {
+      this.logout();
+      return of(false);
+    }
 
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
@@ -63,5 +66,10 @@ export class AuthService {
         return of(false);
       })
     );
+  }
+  logout() {
+    localStorage.removeItem('token');
+    this._currentUser.set(null);
+    this._authStatus.set(AuthStatus.notAuthenticated);
   }
 }
--- src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed } from '@angular/core';
 import { AuthService } from '../../../auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout',
@@ -9,9 +10,14 @@ import { AuthService } from '../../../auth/services/auth.service';
 export class DashboardLayoutComponent {
   public user = computed(() => this.authService.user());
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
   //
   //   get status() {
   //     return this.authService.authStatus;
   //   }
+
+  onLogout() {
+    this.authService.logout();
+    // this.router.navigateByUrl('/auth/login');
+  }
 }
