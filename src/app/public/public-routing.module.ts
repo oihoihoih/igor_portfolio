c630bf7761bfa279d7a79276d06e91157f4312fc
oihoihoih <ochyuo@hotmail.com>
2024-10-07 13:16:06 +0200

    Public routes added

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PublicLayoutComponent,
     children: [
-      { path: '', component: HomeComponent }, // Página principal
+      { path: '', component: HomeComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'projects', component: ProjectsComponent },
       { path: '**', redirectTo: '' }, // Redirigir cualquier otra ruta a la página principal pública
     ],
   },
