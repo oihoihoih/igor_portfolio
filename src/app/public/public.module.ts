d28db9e9edf25db8559586076d389d8203b8bdce
oihoihoih <ochyuo@hotmail.com>
2024-10-07 13:14:00 +0200

    About, contact and projects components added

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -5,13 +5,19 @@ import { PublicRoutingModule } from './public-routing.module';
 import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 
 @NgModule({
   declarations: [
     PublicLayoutComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    ContactComponent,
+    ProjectsComponent
   ],
   imports: [
     CommonModule,
