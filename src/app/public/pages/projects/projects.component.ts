9bbd3b6c00a3dda0d8fd8b8ce32dd6145638390f
oihoihoih <ochyuo@hotmail.com>
2024-10-08 17:30:01 +0200

    Projects component basic html structure added

--- src/app/public/pages/projects/projects.component.ts
+++ src/app/public/pages/projects/projects.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
-
+  constructor() {}
 }
