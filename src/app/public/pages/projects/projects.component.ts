7f8bdfdb033bb2c2a77e7ea4f18961039ee34e1e
oihoihoih <ochyuo@hotmail.com>
2024-10-08 18:01:38 +0200

    Projects component logid applied

--- src/app/public/pages/projects/projects.component.ts
+++ src/app/public/pages/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DashService } from '../../../dashboard/services/dash.service';
+import { Project } from '../../../model/project';
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
-  constructor() {}
+  public projects: Project[] = [];
+  public selectedFileUrl: string | ArrayBuffer | null = null;
+
+  constructor(private dashService: DashService) {
+    this.dashService.getAllProjects().subscribe((res) => {
+      this.projects = res.map((project) => {
+        project.img = this.getImageUrl(project.img);
+        return project;
+      });
+      console.log('proyecto', this.projects);
+    });
+  }
+
+  // Acceso a la URL de la imagen
+  getImageUrl(img: string): string {
+    const backendUrl = 'http://localhost:3000';
+    return `${backendUrl}${img}`;
+  }
 }
