4a2d7557108ca61ce9db5e93dba4a1cb53c7a7c0
oihoihoih <ochyuo@hotmail.com>
2024-10-09 16:33:16 +0200

    Adding Browser Animation Module to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,7 @@ import { heroUsers } from '@ng-icons/heroicons/outline';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NgIconsModule.withIcons({ featherAirplay, heroUsers }),
   ],
