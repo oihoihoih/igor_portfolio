cbcdf4a3911ebdb33eb6d5604d80848e87aabc0e
oihoihoih <ochyuo@hotmail.com>
2025-02-06 18:12:53 +0100

    Enter animation in overlay

--- src/app/public/pages/projects/project-trailer/project-trailer.component.ts
+++ src/app/public/pages/projects/project-trailer/project-trailer.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostBinding } from '@angular/core';
 import { Project } from '../../../../model/project';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-project-trailer',
@@ -7,8 +8,22 @@ import { Project } from '../../../../model/project';
 
   templateUrl: './project-trailer.component.html',
   styleUrl: './project-trailer.component.css',
+  animations: [
+    trigger('animation', [
+      transition(':enter', [
+        style({ opacity: 0, scale: 0.8 }),
+        animate('150ms ease-in-out', style({ opacity: 1, scale: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 0, scale: 1 }),
+        animate('150ms ease-in-out', style({ opacity: 0, scale: 0.8 })),
+      ]),
+    ]),
+  ],
 })
 export class ProjectTrailerComponent {
   @Input({ required: true }) project!: Project;
+  @HostBinding('@animation') animationState = true;
+
   public trailer = 'Trailer url';
 }
